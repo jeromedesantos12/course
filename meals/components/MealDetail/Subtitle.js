823f4ffd0df514c150f5b2ee14389da49d454cc9
Jeremy Santoso <97815661+jeromedesantos12@users.noreply.github.com>
2026-01-05 14:14:02 +0700

    tambah meals

---
+++ meals/components/MealDetail/Subtitle.js
@@ -0,0 +1,29 @@
+import { View, Text, StyleSheet } from "react-native";
+
+function Subtitle({ children }) {
+  return (
+    <View style={styles.subtitleContainer}>
+      <Text style={styles.subtitle}>{children}</Text>
+    </View>
+  );
+}
+
+export default Subtitle;
+
+const styles = StyleSheet.create({
+  subtitle: {
+    color: "#e2b497",
+    fontSize: 18,
+    fontWeight: "bold",
+    margin: 4,
+    textAlign: "center",
+  },
+  subtitleContainer: {
+    padding: 6,
+    margin: 4,
+    marginHorizontal: 24,
+    marginVertical: 4,
+    borderBottomColor: "#e2b497",
+    borderBottomWidth: 2,
+  },
+});
